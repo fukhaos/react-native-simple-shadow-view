84bc4969dcf1fe047e2a6e39900b4a9bbc1f558d
Steven Bell <bellsteven95@gmail.com>
2019-10-24 01:53:41 -0400

    add android x support

--- android/src/main/java/com/reactlibrary/BlurBuilder.java
+++ android/src/main/java/com/reactlibrary/BlurBuilder.java
@@ -3,10 +3,10 @@ package com.como.RNTShadowView;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
-import android.support.v8.renderscript.Allocation;
-import android.support.v8.renderscript.Element;
-import android.support.v8.renderscript.RenderScript;
-import android.support.v8.renderscript.ScriptIntrinsicBlur;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 
 public class BlurBuilder {
 
